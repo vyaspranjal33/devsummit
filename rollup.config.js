5ddd42fa1af16cc206aee37f2c89d4095be728a2
Paul Lewis <github@aerotwist.com>
2016-11-10 15:06:43 -0800

    Timestamps

--- rollup.config.js
+++ rollup.config.js
@@ -13,6 +13,8 @@ const intro = `/*!
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * Build: ${(new Date()).toISOString()}
  */`;
 
 import cleanup from 'rollup-plugin-cleanup';
