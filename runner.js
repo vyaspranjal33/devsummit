33950abffb32aeea0168aeefa85a0ba4ab5d16c1
Sam Thorogood <sam.thorogood@gmail.com>
2018-08-25 09:57:12 +1000

    cleanup

--- runner.js
+++ runner.js
@@ -21,48 +21,42 @@ const ngrok = require('ngrok');
 const process = require('process');
 const { spawn } = require('child_process');
 
-console.log(`env travis: `, process.env.TRAVIS_EVENT_TYPE);
-//console.log(`env variables: `, process.env);
+async function runner() {
+  const url = await ngrok.connect(5000);
+  console.log("ngrok:", url);
 
-async function foobar() {
-    const url = await ngrok.connect(5000);
-    
-    console.log("ngrok:", url);
-    
-    const child = spawn('node', ['app.js']);
-    child.stdout.on('data', (data) => {
-        console.log(`app.js child stdout:\n${data}`);
-        let logText = String(data);
+  const child = spawn('node', ['app.js']);
+  child.stdout.on('data', (data) => {
+    console.log(`app.js child stdout:\n${data}`);
+    const logText = String(data);
+    if (!logText.startsWith('App listening on')) {
+      return;  // not ready yet
+    }
 
-        if (logText.startsWith('App listening on')) {
-            console.log('Server is running. Calling Lighthouse CI.');
+    console.log('Server is running. Calling Lighthouse CI.');
 
-            const lighthouse = spawn('./node_modules/.bin/lighthouse-ci', [url]);
-            lighthouse.stdout.on('data', (data) => {
-                console.log(`lighthouse child stdout:\n${data}`);
-                let lighthouseText = String(data);
-
-                if (lighthouseText.startsWith('Lighthouse CI score')) {
-                    process.exit(0);
-                }
-            });
-
-            lighthouse.on('close', (code) => {
-                console.log(`lighthouse child exited with code: ${code}`);
-                process.exit(code);
-            });
-        }
+    const lighthouse = spawn('./node_modules/.bin/lighthouse-ci', [url]);
+    lighthouse.stdout.on('data', (data) => {
+      console.log(`lighthouse child stdout:\n${data}`);
+      const lighthouseText = String(data);
+      if (lighthouseText.startsWith('Lighthouse CI score')) {
+        process.exit(0);
+      }
     });
 
-    child.on('close', (code) => {
-        console.log(`app.js child exited with code: ${code}`);
-        process.exit(code);
+    lighthouse.on('close', (code) => {
+      console.log(`lighthouse child exited with code: ${code}`);
+      process.exit(code);
     });
+  });
+
+  child.on('close', (code) => {
+    console.log(`app.js child exited with code: ${code}`);
+    process.exit(code);
+  });
 }
 
-var result = foobar();
-
-result.catch((e) => {
-    console.error(e);
-    process.exit(1);
-})
+runner().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
