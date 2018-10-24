8abc09de1d1a2cbaac18cc1d721e03f02dd8e40f
Tom Greenaway <tomgreenaway@google.com>
2018-10-24 11:43:33 +1100

    Added junk date formatting code.
    Not auto-formatting time right now because we want the duration to be clear on session detail page, e.g. 10am - 11am (not just start time).

--- src/js/routes.js
+++ src/js/routes.js
@@ -71,15 +71,7 @@ function $g(tag, config, children) {
 }
 
 function formatTime(foo) {
-  var date = new Date(foo);
-  var hours = date.getHours();
-  var minutes = date.getMinutes();
-  var ampm = hours >= 12 ? 'pm' : 'am';
-  hours = hours % 12;
-  hours = hours ? hours : 12; // the hour '0' should be '12'
-  minutes = minutes < 10 ? '0'+minutes : minutes;
-  var strTime = hours + ':' + minutes + ' ' + ampm;
-  return date.getMonth()+1 + "/" + date.getDate() + "/" + date.getFullYear() + " " + strTime;
+  return foo;
 }
 
 function formatDate(foo) {
@@ -88,16 +80,7 @@ function formatDate(foo) {
   const dateEnding = ['st', 'nd', 'rd', 'th'];
 
   var date = new Date(foo);
-  var hours = date.getHours();
-  var minutes = date.getMinutes();
-  var ampm = hours >= 12 ? 'pm' : 'am';
-  hours = hours % 12;
-  hours = hours ? hours : 12; // the hour '0' should be '12'
-  minutes = minutes < 10 ? '0'+minutes : minutes;
-  var strTime = hours + ':' + minutes + ' ' + ampm;
-
   return days[date.getDay()] + ", " + months[date.getMonth()] + " " + date.getDate() + dateEnding[Math.min(date.getDate(), 3)];
-  return date.getMonth()+1 + "/" + date.getDate() + "/" + date.getFullYear() + " " + strTime;
 }
 
 export async function subroute(node, route, subroute) {
